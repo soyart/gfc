94f38bdcca3ff436ac40ef2444589f45f6db4709
artworkk <prem@cleverse.com>
2022-08-07 01:43:27 +0700

    feat: rename gfc -> gfc-og, and gfc-ng -> gfc, update README, and remove unused CLI args

--- cmd/gfc/main.go
+++ cmd/gfc/main.go
@@ -1,200 +1,21 @@
 package main
 
 import (
-	"bufio"
-	"bytes"
-	"flag"
+	"fmt"
 	"os"
 
+	"github.com/alexflint/go-arg"
+
+	"github.com/artnoi43/gfc/pkg/usecase/cli"
 	"github.com/artnoi43/gfc/pkg/usecase/gfc"
 )
 
-/* Command-line flags */
-type flags struct {
-	decrypt             bool
-	rsa                 bool
-	useKey              bool
-	stdin, stdout       bool
-	usesBase64, usesHex bool // If both, use Base64. See input() and output()
-	aesMode             string
-	infile, outfile     gfc.File
-	aesKeyFile          gfc.KeyFile
-	pubFile, priFile    gfc.KeyFile
-}
-
 func main() {
-	f := new(flags)
-	f.parseFlags()
-	output(crypt(input(f)))
-}
+	var args = new(cli.Args)
+	arg.MustParse(args)
 
-func (f *flags) parseFlags() {
-	flag.BoolVar(&f.decrypt, "d", false, "Decrypt")
-	flag.BoolVar(&f.rsa, "rsa", false, "Use RSA encryption")
-	flag.BoolVar(&f.useKey, "k", false, "Use keyfile for AES or RSA")
-	flag.BoolVar(&f.stdin, "stdin", false, "Get input from stdin")
-	flag.BoolVar(&f.stdout, "stdout", false, "Direct output to stdout")
-	flag.BoolVar(&f.usesBase64, "B", false, "Base64 encoding/decoding")
-	flag.BoolVar(&f.usesHex, "H", false, "Hexadecimal encoding/decoding")
-	flag.StringVar(&f.aesMode, "m", "GCM", "AES modes (GCM or CTR)")
-	flag.StringVar(&f.infile.Name, "i", "", "Input file")
-	flag.StringVar(&f.outfile.Name, "o", "./delete.me", "Output file")
-	flag.StringVar(&f.aesKeyFile.Name, "f", "", "AES key file")
-	flag.StringVar(&f.pubFile.Name, "pub", "", "RSA public key file")
-	flag.StringVar(&f.priFile.Name, "pri", "", "RSA private key file")
-
-	flag.Parse()
-
-	if f.infile.Name == "" && !f.stdin {
-		gfc.Write(os.Stderr, "no input file\n")
-		os.Exit(1)
-	}
-
-	// RSA key is handled in handleRSA
-	if f.useKey {
-		if f.aesKeyFile.Name == "" {
-			keyFilename, found := os.LookupEnv("AES_KEY")
-			if !found {
-				f.aesKeyFile.Name = keyFilename
-			} else {
-				gfc.Write(os.Stderr, "no AES key file\n")
-				os.Exit(1)
-			}
-		}
+	if err := args.Handle(); err != nil {
+		gfc.Write(os.Stderr, fmt.Sprintf("error: %s\n", err.Error()))
+		os.Exit(2)
 	}
 }
-
-// handleAES prepares key for AES and calls the AES encryption/decryption functions
-func handleAES(f *flags, ibuf gfc.Buffer) (aesOut gfc.Buffer) {
-	var aesKey []byte
-	var err error
-	/* Read AES keyfile - if empty, passphrase will be used */
-	if f.useKey {
-		aesKey = f.aesKeyFile.ReadKey()
-	}
-	switch f.aesMode {
-	case "CTR", "ctr":
-		if f.decrypt {
-			aesOut, err = gfc.DecryptCTR(ibuf, aesKey)
-		} else {
-			aesOut, err = gfc.EncryptCTR(ibuf, aesKey)
-		}
-	case "GCM", "gcm":
-		if f.decrypt {
-			aesOut, err = gfc.DecryptGCM(ibuf, aesKey)
-		} else {
-			aesOut, err = gfc.EncryptGCM(ibuf, aesKey)
-		}
-	default:
-		gfc.Write(os.Stderr, "Invalid AES mode - only GCM or CTR is supported\n")
-		os.Exit(1)
-	}
-	if err != nil {
-		gfc.Write(os.Stderr, "AES crypt error: "+err.Error()+"\n")
-	}
-	return aesOut
-}
-
-// handleRSA prepares the RSA keypair and calls RSA encryption/decryption functions
-func handleRSA(f *flags, ibuf gfc.Buffer) (rsaOut gfc.Buffer) {
-	var err error
-	if f.decrypt {
-		var priKey []byte
-		if f.useKey {
-			priKey = f.priFile.ReadKey()
-		} else {
-			priKey = []byte(os.Getenv("PRI"))
-		}
-		switch len(priKey) {
-		case 0:
-			gfc.Write(os.Stderr, ErrMsgNoPri)
-			os.Exit(1)
-		default:
-			rsaOut, err = gfc.DecryptRSA(ibuf, priKey)
-		}
-	} else {
-		var pubKey []byte
-		if f.useKey {
-			pubKey = f.pubFile.ReadKey()
-		} else {
-			pubKey = []byte(os.Getenv("PUB"))
-		}
-		switch len(pubKey) {
-		case 0:
-			gfc.Write(os.Stderr, ErrMsgNoPub)
-			os.Exit(1)
-		default:
-			rsaOut, err = gfc.EncryptRSA(ibuf, pubKey)
-		}
-	}
-	if err != nil {
-		gfc.Write(os.Stderr, "RSA crypt error: "+err.Error()+"\n")
-	}
-	return rsaOut
-}
-
-func input(f *flags) (*flags, gfc.Buffer) {
-	var ibuf gfc.Buffer
-	/* Read from stdin or file */
-	if f.stdin {
-		scanner := bufio.NewScanner(os.Stdin)
-		scanner.Scan()
-		ibuf = bytes.NewBuffer([]byte(scanner.Text()))
-	} else {
-		ibuf = f.infile.ReadFile()
-	}
-	/* Decode input when decrypting */
-	if f.decrypt {
-		var err error
-		if f.usesBase64 {
-			ibuf, err = gfc.Decode(gfc.Base64, ibuf)
-		} else if f.usesHex {
-			ibuf, err = gfc.Decode(gfc.Hex, ibuf)
-		}
-		if err != nil {
-			gfc.Write(os.Stderr, "input decoding failed")
-		}
-	}
-	/* Return (un)processed input buffer */
-	return f, ibuf
-}
-
-func crypt(f *flags, ibuf gfc.Buffer) (*flags, gfc.Buffer) {
-	var obuf gfc.Buffer
-	if f.rsa {
-		obuf = handleRSA(f, ibuf)
-	} else {
-		obuf = handleAES(f, ibuf)
-	}
-	/* Return encrypted/decrypted buffer */
-	return f, obuf
-}
-
-func output(f *flags, obuf gfc.Buffer) {
-	/* Encode output when encrypting */
-	if !f.decrypt {
-		var err error
-		if f.usesBase64 {
-			obuf, err = gfc.Encode(gfc.Base64, obuf)
-		} else if f.usesHex {
-			obuf, err = gfc.Encode(gfc.Hex, obuf)
-		}
-		if err != nil {
-			gfc.Write(os.Stderr, "output encoding failed")
-			os.Exit(2)
-		}
-	}
-	var outfile *os.File
-	/* Write to stdout or file */
-	if f.stdout {
-		outfile = os.Stdout
-	} else {
-		outfile = f.outfile.Create()
-	}
-	obuf.WriteTo(outfile)
-}
-
-const (
-	ErrMsgNoPub = "No public key specified\nUse '-k -pub <path>', or environment variable PUB to specify RSA public key\n"
-	ErrMsgNoPri = "No private key specified\nUse '-k -pri <path>', or environment variable PRI to specify RSA private key\n"
-)
--- pkg/usecase/cli/args.go
+++ pkg/usecase/cli/args.go
@@ -2,12 +2,10 @@ package cli
 
 type baseCryptFlags struct {
 	DecryptFlag  bool   `arg:"-d,--decrypt" default:"false" help:"Decrypt mode"`
-	StdinFlag    bool   `arg:"--stdin" default:"false" help:"Use stdin as input"`
-	StdoutFlag   bool   `arg:"--stdout" default:"false" help:"Write output to stdout"`
 	StdinText    bool   `arg:"-t,--text" default:"false" help:"Enter a text line manually to stdin"`
-	InfileFlag   string `arg:"-i,--infile" placeholder:"IN" help:"Input filename, stdin if omitted"`
-	EncodingFlag string `arg:"-e,--encoding" placeholder:"ENC" help:"'base64' or 'hex' encoding for input or output"`
-	OutfileFlag  string `arg:"-o,--outfile" placeholder:"OUT" help:"Output filename, stdout if omitted"`
+	InfileFlag   string `arg:"-i,--infile" placeholder:"IN" help:"Input filename, stdin will be used if omitted"`
+	EncodingFlag string `arg:"-e,--encode" placeholder:"ENC" help:"'base64' or 'hex' encoding for input or output"`
+	OutfileFlag  string `arg:"-o,--outfile" placeholder:"OUT" help:"Output filename, stdout will be used if omitted"`
 }
 
 type Args struct {

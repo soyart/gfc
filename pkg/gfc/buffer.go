b0c33d0bea00731c7032098f34e6219010cd9b19
artnoi <artdrawin@gmail.com>
2022-08-14 01:49:55 +0700

    refactor(gfc): use interfaces from package io

--- pkg/gfc/buffer.go
+++ pkg/gfc/buffer.go
@@ -5,9 +5,9 @@ import "io"
 // type Buffer *bytes.Buffer
 
 type Buffer interface {
-	Read([]byte) (int, error)
-	Write([]byte) (int, error)
-	ReadFrom(io.Reader) (int64, error)
-	WriteTo(io.Writer) (int64, error)
+	io.Reader
+	io.Writer
+	io.ReaderFrom
+	io.WriterTo
 	Bytes() []byte
 }

533a566681afb42374e3a4e3f8e6721c70d6d10f
artworkk <prem@cleverse.com>
2023-04-11 00:34:17 +0200

    nothing, really

--- cmd/gfc/main.go
+++ cmd/gfc/main.go
@@ -10,9 +10,9 @@ import (
 )
 
 const (
-	// Exit statuses for different errors
-	otherError int = 1
-	userError  int = 2
+	_          = iota // 0
+	otherError        // 1
+	userError         // 2
 
 	// These strings will be concatenated with error message
 	// when writing out to stderr after an error occured in main
@@ -21,34 +21,28 @@ const (
 )
 
 func main() {
-	var gfcCli = new(cli.Gfc)
+	gfcCli := new(cli.Gfc)
 	arg.MustParse(gfcCli)
 
 	if err := gfcCli.Run(); err != nil {
-		// The cases inside the switch block is user error,
-		// and gfc will exit with status 1
 		switch {
-		case errors.Is(err, cli.ErrMissingSubcommand):
-			fallthrough
-		case errors.Is(err, cli.ErrFileIsDir):
-			fallthrough
-		case errors.Is(err, cli.ErrOutfileNotWritable):
-			fallthrough
-		case errors.Is(err, cli.ErrBadInfileIsText):
-			fallthrough
-		case errors.Is(err, cli.ErrBadOutfileDir):
-			fallthrough
-		case errors.Is(err, cli.ErrInvalidModeAES):
-			die(userError, err.Error())
-		}
+		case
+			errors.Is(err, cli.ErrMissingSubcommand),
+			errors.Is(err, cli.ErrFileIsDir),
+			errors.Is(err, cli.ErrOutfileNotWritable),
+			errors.Is(err, cli.ErrBadInfileIsText),
+			errors.Is(err, cli.ErrBadOutfileDir),
+			errors.Is(err, cli.ErrInvalidModeAES):
 
-		// Non-user error
-		die(otherError, err.Error())
+			die(userError, err.Error())
+
+		default:
+			die(otherError, err.Error())
+		}
 	}
 }
 
 func die(exitStatus int, msg string) {
-	// Concat strings, bc why not?
 	errStr := errorMsg + msg + "\n"
 
 	switch exitStatus {
